7bf89353e35210582a31dcf57a5ced4a40a8f09b
Bendsley95 <bendsley95@gmail.com>
2018-08-27 10:02:59 -0600

    copy over previous assessment for base

---
+++ src/main/java/com/example/TestApplication/TestApplication.java
@@ -0,0 +1,16 @@
+package com.example.TestApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.example.TestApplication"})
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+}
